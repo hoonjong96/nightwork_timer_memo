842c396df1d0c949f7a29b8b3ed547cf7905a8d7
jongdroid <qkr7627@gmail.com>
2022-05-13 02:22:03 +0900

    2022.05.13 02:18am jongdroid commit What Changed - Changed Variable Name and Deleted - add Dispose Function - Changed Title Text - setState 내부 함수 모두 정리 - 시간 저장 btn onPressed event 정리

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:nightwork_timer_memo/bottom.dart';
 import 'package:nightwork_timer_memo/cardview.dart';
 import 'package:nightwork_timer_memo/secondpage.dart';
 
-
 /** 2022-05-06 code design: jongdroid
  *  해당 서비스는 타이머로 시간을 기록하고, 간단한 메모를 로컬에 저장할 수 있음
  *  해당 페이지에서는 타이머와 관련된 로직을 작성하고, 시간 저장을 하였을때 다음 페이지로 타이머 시간을 전달함
@@ -17,25 +16,13 @@ void main() {
   runApp(const MyApp());
 }
 
-var now_hour = DateFormat('HH').format(DateTime.now());
-var now_min = DateFormat('mm').format(DateTime.now());
-var nowTime = '${now_hour}시 ${now_min}분';
-var nowTimeList = ['야근 시작 시간', '$nowTime'];
+var today_date = DateFormat('yyyy.MM.dd').format(DateTime.now()); //오늘 날짜 저장 변수
+var showNowState = '대기중'; //현재 상태를 보여줌(야근 시작 / 중지)
 
-var end_hour = DateFormat('HH').format(DateTime.now());
-var end_min = DateFormat('mm').format(DateTime.now());
-var endTime = '${end_hour}시 ${end_min}분';
-var endTimeList = ['야근 종료 시간', '$endTime'];
-
-var saveTime;
-
-var getState = '대기중';
-
-// 넘겨주는 데이터
-var getNowTime = '대기중';
-var sendEndTime = '대기중';
-var saveEndTime = '대기중';
-var sendWorkTime = '대기중';
+// 다음페이지로 넘겨주는 데이터, 대기중은 초기값을 의미
+var saveNowTime = '대기중'; // 야근 시작 버튼 눌렀을때 시점의 시간 저장
+var saveWorkTime = '대기중'; // 야근한 시간 저장
+var saveEndTime = '대기중'; // 야근 종료한 시점의 시간을 저장
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -43,7 +30,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       routes: {
         '/home': (context) => MyApp(),
         '/data': (context) => SaveTodayData(),
@@ -61,26 +47,23 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-//현재 날짜 출력 년-월-일 format
-var today_date = DateFormat('yyyy.MM.dd').format(DateTime.now());
-
 class _HomeScreenState extends State<HomeScreen> {
   late Timer timer;
-  var second = 0; //타이머의 초
-  var min = 0; // 분
-  var hour = 0; // 시간
+  var second = 0;
+  var min = 0;
+  var hour = 0;
 
-  var nowTimeIndex = 0;
-  var endTimeIndex = 0;
-
-  var saveDataList = [];
-  var counterTime;
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('오늘의 야근'),
+        title: Text('야근 타이머'),
         centerTitle: true,
       ),
       body: Center(
@@ -104,8 +87,10 @@ class _HomeScreenState extends State<HomeScreen> {
                   children: [
                     Text(
                       today_date,
-                      style: TextStyle( fontSize: 18,
-                          color: Colors.white, fontWeight: FontWeight.bold),
+                      style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
                     ),
                   ],
                 ),
@@ -116,9 +101,9 @@ class _HomeScreenState extends State<HomeScreen> {
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
                     Text(
-                      '${getState}',
+                      showNowState,
                       style: TextStyle(
-                          color: Colors.black54, fontWeight: FontWeight.bold),
+                          color: Colors.indigo, fontWeight: FontWeight.bold),
                     ),
                   ],
                 ),
@@ -130,8 +115,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   children: [
                     Text(
                       '$hour',
-                      style:
-                      TextStyle(fontSize: 60, color: Color(0xff448AFF)),
+                      style: TextStyle(fontSize: 60, color: Color(0xff448AFF)),
                     ),
                     Text(
                       '시간',
@@ -139,8 +123,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     ),
                     Text(
                       '$min',
-                      style:
-                      TextStyle(fontSize: 60, color: Color(0xff448AFF)),
+                      style: TextStyle(fontSize: 60, color: Color(0xff448AFF)),
                     ),
                     Text(
                       '분',
@@ -148,8 +131,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     ),
                     Text(
                       '$second',
-                      style:
-                      TextStyle(fontSize: 60, color: Color(0xff448AFF)),
+                      style: TextStyle(fontSize: 60, color: Color(0xff448AFF)),
                     ),
                     Text(
                       '초',
@@ -160,23 +142,19 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
               Text(
                 '오늘도 힘내세요!',
-                style: TextStyle( fontSize: 16,
-                    color: Colors.grey, fontWeight: FontWeight.bold),
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.blueGrey,
+                    fontWeight: FontWeight.bold),
               ),
-
-              Container( height:1.0, margin: EdgeInsets.all(3),
-                width:500.0,
-                color:Colors.grey,),
-
-              
               Container(
-                margin: EdgeInsets.all(8),
+                height: 1.0,
+                margin: EdgeInsets.all(3),
+                width: 500.0,
+                color: Colors.grey,
               ),
-              
               ButtonBar(
-                alignment:
-                MainAxisAlignment.spaceEvenly, //ButtonBar 버튼들이 중앙으로 이동
-
+                alignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   ElevatedButton(
                     child: Text(
@@ -184,13 +162,11 @@ class _HomeScreenState extends State<HomeScreen> {
                       style: TextStyle(fontSize: 16),
                     ),
                     onPressed: () {
-
-                      setState(() {
-                        getNowTime = DateFormat('hh:mm').format(DateTime.now());
-                        getState = "야근중";
-                        startTimerBtn();
-                        Navigator.pushNamed(context, '/DetailWork', arguments: getNowTime);
-                      });
+                      saveNowTime = DateFormat('HH:mm').format(DateTime.now());  // 야근 시작 버튼을 눌렀을때 즉시 시간 기록(시:분만 저장)
+                      showNowState = "야근중"; // 현재 상태를 저장하는 변수
+                      startTimerBtn(); // 타이머 시작 함수 호출
+                      Navigator.pushNamed(context, '/DetailWork', // 다음 페이지로 위에서 저장한 야근 버튼 눌렀을때 시간 전달
+                          arguments: saveNowTime);
                     },
                   ),
                   ElevatedButton(
@@ -199,10 +175,9 @@ class _HomeScreenState extends State<HomeScreen> {
                       style: TextStyle(fontSize: 16),
                     ),
                     onPressed: () {
-                      setState(() {
-                        getState = "중지";
-                        pushEndBtn();
-                      });
+                      showNowState = "중지";
+                      timer.cancel();
+                      setState(() {});
                     },
                   ),
                   ElevatedButton(
@@ -211,17 +186,17 @@ class _HomeScreenState extends State<HomeScreen> {
                       style: TextStyle(fontSize: 16),
                     ),
                     onPressed: () {
-                      ShowToast();
-                      pushResetBtn();
-                      setState(() {
-                        getState = "저장완료";
-                        sendWorkTime = '${hour}시간 ${min}분';
-                        saveEndTime = DateFormat('hh:mm').format(DateTime.now());
-                        today_date = DateFormat('yyyy.MM.dd').format(DateTime.now());
-                        Navigator.pushNamed(context, '/DetailWork', arguments: sendEndTime);
-                        Navigator.pushNamed(context, '/DetailWork', arguments: sendWorkTime);
-                        Navigator.pushNamed(context, '/DetailWork', arguments: today_date);
-                      });
+                      showSaveMessageToast(); //저장 완료를 알림
+                      showNowState = "저장완료"; //저장 완료 상태임을 나타냄
+                      saveWorkTime = '${hour}시간 ${min}분'; // 타이머로 측정한 시간을 시간과 분만 저장함
+                      saveEndTime = DateFormat('HH:mm').format(DateTime.now()); // 야근을 종료한 시간을 기록함
+                      resetTimerBtn(); // 타이머를 중단하고, 타이머 초기화
+
+                      // 다음페이지로 저장 버튼을 누른 당일의 날짜, 야근한 시간 데이터 전달
+                      Navigator.pushNamed(context, '/DetailWork',
+                          arguments: today_date);
+                      Navigator.pushNamed(context, '/DetailWork',
+                          arguments: saveWorkTime);
                     },
                   ),
                 ],
@@ -250,11 +225,7 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
-  void pushEndBtn() {
-    timer.cancel();
-  }
-
-  void pushResetBtn() {
+  void resetTimerBtn() {
     setState(() {
       hour = 0;
       second = 0;
@@ -263,7 +234,7 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
-  void ShowToast() {
+  void showSaveMessageToast() {
     Fluttertoast.showToast(
       msg: '저장을 완료하였습니다.',
       gravity: ToastGravity.BOTTOM,
@@ -273,5 +244,4 @@ class _HomeScreenState extends State<HomeScreen> {
       toastLength: Toast.LENGTH_SHORT,
     );
   }
-
 }
