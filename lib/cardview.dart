d3797c233c5d0364e8432960bc2cf4c7269435c5
jongdroid <qkr7627@gmail.com>
2022-06-14 00:07:57 +0900

    2022.06.13 23:35 jongdroid - 불필요한 코드 및 주석제거

--- lib/cardview.dart
+++ lib/cardview.dart
@@ -5,7 +5,6 @@ import 'package:nightwork_timer_memo/bottom.dart';
 import 'package:nightwork_timer_memo/main.dart';
 import 'package:nightwork_timer_memo/secondpage.dart';
 
-
 /** 2022-05-06 code design: jongdroid
  *  해당 페이지는 일일 기록으로 부터 전달받은 데이터를 누적하여 기록한다.
  *  [개선사항] -> 해당 페이지에서 그래프와 같은 수단을 통하여 일일/주간/월간 단위로 야근 통계를 볼 수 있도록 개선 필요
@@ -31,24 +30,21 @@ class CardView extends StatelessWidget {
   }
 }
 
-
-
 class CardViewScreen extends StatefulWidget {
 
-  var today_date;  // 오늘 날짜 받아옴
-  var saveEndTime; //
-  var sendWorkTime; // 야근 시간
+  // 이전페이지에서 받아온 데이터입니다. (날짜, 데이터저장한시간, 야근한 시간, 야근 입력 텍스트)
+  var today_date;
+  var saveEndTime;
+  var saveWorkTime;
   var titleController;
-
 
   CardViewScreen(
       {Key? key,
         this.saveEndTime,
-        this.sendWorkTime,
+        this.saveWorkTime,
         this.titleController,
         this.today_date})
       : super(key: key);
-
 
   @override
   _CardViewScreen createState() => _CardViewScreen();
@@ -56,73 +52,66 @@ class CardViewScreen extends StatefulWidget {
 
 class _CardViewScreen extends State<CardViewScreen> {
 
-  var myList = <String>[]; // 날짜데이터
-  var sendWorkTimeList = <String>[];
-  var nowTimeList = <String>[];
+  // 야근 시간 저장한 날짜 저장 리스트
+  List<String> dateList = [];
+
+  // 야근한 시간을 저장하는 리스트
+  List<String> worktimeList = [];
+
+  // 야근 시간을 저장한 날짜의 시간 저장 리스트
+  // 예 - 23:58분에 저장했다면 해당 시간 기록
+  List<String> getNowtimeList = [];
+
+  // 야근 task 기록 text save list
+  List<String> titleList = <String>[];
+
   var nowTimeVar; // 현재 시간 저장 데이터
-
   var titleController = TextEditingController();
-  var titleList = <String>[];
-
-  //
-  // var textDatailDataList = <String>[];
-  //
-  // var textController = TextEditingController();
-
-
 
   @override
   void initState() {
     super.initState();
-    _loadCounter();
+    getDataList();
   }
 
-  _loadCounter() async {
+  getDataList() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
 
     setState(() {
-      myList = (prefs.getStringList("myList"))!;
-      prefs.setStringList("myList", myList);
+      dateList = (prefs.getStringList("myList"))!;
+      prefs.setStringList("myList", dateList);
 
-      sendWorkTimeList = (prefs.getStringList("worktimekey"))!;
-      prefs.setStringList("worktimekey", sendWorkTimeList);
+      worktimeList = (prefs.getStringList("worktimekey"))!;
+      prefs.setStringList("worktimekey", worktimeList);
 
-      nowTimeList = (prefs.getStringList("nowtimekey"))!;
-      prefs.setStringList("nowtimekey", nowTimeList);
+      getNowtimeList = (prefs.getStringList("nowtimekey"))!;
+      prefs.setStringList("nowtimekey", getNowtimeList);
 
       titleList = (prefs.getStringList("titlekey"))!;
       prefs.setStringList("titlekey", titleList);
-
-      print('$myList' + 'test임');
     });
   }
 
-  _incrementCounter() async {
+  setDataList() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
 
     setState(() {
 
+      dateList.add(today_date);
+      prefs.setStringList("myList", dateList);
 
-
-      myList.add(today_date);
-      prefs.setStringList("myList", myList);
-
-      sendWorkTimeList.add(sendWorkTime);
-      prefs.setStringList("worktimekey", sendWorkTimeList);
+      worktimeList.add(saveWorkTime);
+      prefs.setStringList("worktimekey", worktimeList);
 
       nowTimeVar = DateFormat('HH:mm:ss').format(DateTime.now());
-      nowTimeList.add('${nowTimeVar}');
-      prefs.setStringList("nowtimekey", nowTimeList);
-
+      getNowtimeList.add('${nowTimeVar}');
+      prefs.setStringList("nowtimekey", getNowtimeList);
 
       titleList.add(titleController.text);
       prefs.setStringList("titlekey", titleList);
 
     });
   }
-
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -133,46 +122,24 @@ class _CardViewScreen extends State<CardViewScreen> {
       ),
       body: ListView.separated(
         padding: EdgeInsets.all(8),
-        itemCount: myList.length,
+        itemCount: dateList.length,
         itemBuilder: (context, index) {
           return Card(
             margin: EdgeInsets.all(25),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
-
                 ListTile(
-                 
                   title: Text('${titleList[index]}'),
-
                   subtitle: Text(
-                    '날짜-${myList[index]} ${nowTimeList[index]}'
-                        ' 야근시간- ${sendWorkTimeList[index]}',
+                    '날짜-${dateList[index]} ${getNowtimeList[index]}'
+                        ' 야근시간- ${worktimeList[index]}',
                     style: TextStyle(
                         color: Colors.blue,
                         fontSize: 14,
                         fontWeight: FontWeight.normal),
                   ),
                 ),
-
-
-                // ButtonBar(
-                //   children: <Widget>[
-                //     TextButton(
-                //       child: Text('저장'),
-                //       onPressed: () {
-                //         // _incrementCounter();
-                //         // showPerformanceDialog(context);
-                //
-                //         // setState(() {
-                //         //
-                //         // });
-                //
-                //
-                //       },
-                //     )
-                //   ],
-                // )
               ],
             ),
           );
@@ -184,33 +151,4 @@ class _CardViewScreen extends State<CardViewScreen> {
       bottomNavigationBar: MenuBottom(),
     );
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
